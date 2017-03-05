ec38a84e0f223730318f26a82d151f0e0c669a78
Christine Yen <cyen@christineyen.com>
2017-03-04 18:54:48 -0800

    Initialize gh-pages with pdoc output

--- examples/django_response_time/django_response_time/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_response_time.settings")
-
-application = get_wsgi_application()
